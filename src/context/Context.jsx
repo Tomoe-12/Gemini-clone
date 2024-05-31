fb589f4c749d65d35a635a7a9cc369d958bd68f4
Tomoe-12 <li8993han@gmail.com>
2024-05-31 13:34:58 +0630

    doing responsive andadding state for the question

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,17 +13,48 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState('')
 
-    const onSent = async () => {
+    const delayParam = (index, nextWork) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWork)
+        }, 75 * index)
+    }
+
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
+    const onSent = async (prompt) => {
+
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input)
-        setResultData(response)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt)
+            setRecentPrompt(prompt)
+        } else {
+            setPrevPrompts(prev => [...prev, input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+        let responseArray = response.split("**")
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i]
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>"
+            }
+        }
+        let newResponse2 = newResponse.split('*').join('</br>')
+        let newResponseArray = newResponse2.split(" ")
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i]
+            delayParam(i, nextWord + " ")
+        }
         setLoading(false)
         setInput('')
-       
-
     }
 
 
@@ -38,6 +69,7 @@ const ContextProvider = (props) => {
         resultData,
         input,
         setInput,
+        newChat,
     }
 
     return (
